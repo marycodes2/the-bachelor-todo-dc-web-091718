d56e80e1ddbb2b83766f72fc9b3036413f3978cf
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-24 18:22:47 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -39,6 +39,7 @@ bachelor_data = {
 def get_first_name_of_season_winner(data, season)
   binding.pry
   data[season].each do |contestant_data|
+    puts data[season].class
     if contestant_data[:status] == "Winner"
     #if season_name.to_s == season
       #season_data.each do |contestant_line|
