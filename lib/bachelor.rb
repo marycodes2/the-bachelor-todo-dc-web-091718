a87cd53553760b5468f67776abf7145806dc7e36
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-24 18:04:40 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -87,7 +87,7 @@ def get_occupation(data, hometown)
       season_data.each do |contestant_line|
         contestant_line.each do |contestant_attributes, contestant_data| 
           if contestant_attributes = :hometown && contestant_data == hometown
-            puts contestant_line[:name]
+            puts contestant_line[:occupation]
           end
         end
       end
