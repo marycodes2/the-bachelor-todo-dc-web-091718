0776ba9176062dcfb6ab3698316bf83b7778dea4
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-24 18:57:58 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -107,6 +107,7 @@ def get_average_age_for_season_array(data, season)
       season_data.each do |contestant_line|        
         contestant_line.each do |contestant_attributes, contestant_data| 
           if contestant_attributes == :age
+            puts contestant_attributes
             age_of_contestants_in_season << contestant_attributes
           end
         end
