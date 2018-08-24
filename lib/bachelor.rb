1b5096c8c759215255c4b211e26a84ab4644b7fe
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-24 18:03:18 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -83,7 +83,11 @@ end
 puts count_contestants_by_hometown(bachelor_data, 'Denver, Colorado')
 
 def get_occupation(data, hometown)
-  
+  data.each do |season_name, season_data|
+      season_data.each do |contestant_line|
+        contestant_line.each do |contestant_attributes, contestant_data| 
+          if contestant_attributes = :hometown && contestant_data == hometown
+            contestant_line[:name]
 end
 
 def get_average_age_for_season(data, season)
