79024c55b9aab6bb11c56bdf045c0461423d5eaf
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-24 18:53:57 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -115,7 +115,6 @@ def get_average_age_for_season_array(data, season)
   end
 end
 return age_of_contestants_in_season
-end
 
 def get_average_age_for_season(data, season)
   array = get_average_age_for_season_array
