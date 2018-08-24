476f17d65c1fc90900d0a76e87f9e31aae229adc
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-24 17:58:04 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -67,7 +67,9 @@ end
 puts get_contestant_name(bachelor_data, "Chiropractic Assistant")
 
 def count_contestants_by_hometown(data, hometown)
-  
+  data.each do |season_name, season_data|
+      season_data.each do |contestant_line|
+        contestant_line.each do |contestant_attributes, contestant_data|  
 end
 
 def get_occupation(data, hometown)
