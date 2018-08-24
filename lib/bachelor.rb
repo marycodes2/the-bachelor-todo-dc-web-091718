322a93e7e12df1564e8433498af49c5e44b82216
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-24 17:44:46 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -43,7 +43,7 @@ def get_first_name_of_season_winner(data, season)
             name = contestant_line[:name]
             name = name.split(" ")
             name = name[0]
-            return name
+            return name.class
           end 
         end 
       end 
